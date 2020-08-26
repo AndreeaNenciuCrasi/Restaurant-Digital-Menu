2b2e7631c3fc0bae11bf4f488fe2d47382ef2843
iulian-coder <iulian.titei@gmail.com>
2020-08-26 10:21:56 +0300

    move redirect user signup

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,48 +1,101 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Redirect} from "react-router";
+import { Redirect } from "react-router";
 
 export default function UserSignUp() {
   const { register, handleSubmit } = useForm();
-  const [toHome, setToHome] = useState(false);
+  const [toHome, setToHome] = useState();
 
   const onSubmit = (data) => {
-    setToHome(true);
     fetch(" http://localhost:8080/api/v2/user", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
-    }).then((res) => res.json());
-  }
+    }).then((response) => {
+      if (response.status === 200) {
+        setToHome(true);
+      }
+    });
+  };
 
   return (
     <div>
       {toHome ? <Redirect to="/" /> : null}
-      <div className="container-register" style={{ width: "38rem", marginTop: "50px", justifyContent : "center", display : "flex" }}>
+      <div
+        className="container-register"
+        style={{
+          width: "38rem",
+          marginTop: "50px",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
         <form onSubmit={handleSubmit(onSubmit)}>
-          <h5 style = {{ color : "blue" }}>Register account</h5>
+          <h5 style={{ color: "blue" }}>Register account</h5>
           <div class="form-group">
             {/* <label for="First Name">First Name</label> */}
-            <input name="firstName" placeholder="First name" ref={register} type="text" class="form-control" id="first-name"/>
+            <input
+              name="firstName"
+              placeholder="First name"
+              ref={register}
+              type="text"
+              class="form-control"
+              id="first-name"
+            />
           </div>
           <div class="form-group">
-            <input name="lastName" placeholder="Last name" ref={register} type="text" class="form-control" id="last-name"/>
+            <input
+              name="lastName"
+              placeholder="Last name"
+              ref={register}
+              type="text"
+              class="form-control"
+              id="last-name"
+            />
           </div>
           <div class="form-group">
-            <input name="userName" placeholder="Username" ref={register} type="text" class="form-control" id="user-name"/>
+            <input
+              name="userName"
+              placeholder="Username"
+              ref={register}
+              type="text"
+              class="form-control"
+              id="user-name"
+            />
           </div>
           <div class="form-group">
-            <input name="emailAddress" placeholder="Email Address" ref={register} type="text" class="form-control" id="email-address"/>
+            <input
+              name="emailAddress"
+              placeholder="Email Address"
+              ref={register}
+              type="text"
+              class="form-control"
+              id="email-address"
+            />
           </div>
           <div class="form-group">
-            <input type="password" name="password" placeholder="Password" ref={register}  class="form-control" id="password"/>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              ref={register}
+              class="form-control"
+              id="password"
+            />
           </div>
           <div class="form-group">
-            <input type="password" name="confirmPassword" placeholder="Confirm password" ref={register} class="form-control" id="confirm-password"/>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
+              ref={register}
+              class="form-control"
+              id="confirm-password"
+            />
           </div>
-          <input type="submit" class="btn btn-primary"/>
+          <input type="submit" class="btn btn-primary" />
         </form>
       </div>
-    </div> 
+    </div>
   );
 }
