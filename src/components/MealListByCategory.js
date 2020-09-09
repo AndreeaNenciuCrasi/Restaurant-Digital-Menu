81a9410cc731aa2e9d921427d8478965fcfe317b
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-09-09 14:18:58 +0300

    Small change

--- src/components/MealListByCategory.js
+++ src/components/MealListByCategory.js
@@ -21,12 +21,12 @@ function MealListByCategory({ name }) {
   return (
     <div className="container FoodCategoriesContainer">
       {foodListCategories.map((item) => (
-          <div
-            key={item.strMeal}
-            className="card FoodCategoriesCard"
-            style={{ width: "18rem", display: "inline-block" }}
-          >
-            <img
+        <div
+          key={item.strMeal}
+          className="card FoodCategoriesCard"
+          style={{ width: "18rem", display: "inline-block" }}
+        >
+          <img
             className="card-img-top cardImg"
             src={item.strMealThumb}
             alt="Card image cap"
@@ -34,8 +34,8 @@ function MealListByCategory({ name }) {
           <div className="card-body">
             <h5 className="card-title">{item.strMeal.substring(0, 25)}</h5>
             <button type="button" onClick={() => faveClick(item)}>
-              <h5 >
-                <FaHeart style = {{ "color" : "blue" }}/>
+              <h5>
+                <FaHeart style={{ color: "blue" }} />
               </h5>
             </button>{" "}
             <Link to={`/food-details/${item.idMeal}`}>
@@ -71,11 +71,14 @@ function handleClick(data) {
 function faveClick(favoriteMeal) {
   const username = window.sessionStorage.getItem("User");
   alert("Meal added to your favorites!");
-  fetch(`http://localhost:8080/api/v2/user/${username}/favorites/${favoriteMeal.idMeal}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(favoriteMeal),
-  }).then((response) => {
+  fetch(
+    `http://localhost:8080/api/v2/user/${username}/favorites/${favoriteMeal.idMeal}`,
+    {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(favoriteMeal),
+    }
+  ).then((response) => {
     console.log(response);
   });
 }
